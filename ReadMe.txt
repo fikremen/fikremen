9818fba3bc598b02648cb2b9d4bc0fd868921cc7
fikremen <70618860+fikremen@users.noreply.github.com>
2022-09-02 19:26:40 -0400

    Adding captain americas mission game
    Adding all files for the interactive graphical game coded in C. This game works on a game boy advance emulator

---
+++ ReadMe.txt
@@ -0,0 +1,5 @@
+You are captain America, get ready to embark on your most difficult challenge yet:
+SAVE CHRIS ROCK FROM THE SLAP.
+Press Start to start the game.
+You control the shield with the arrow buttons and you goal is the keep the hand from slapping Chris's face. Keep Chris happy by running out the clock before you have 0 lives left.
+Good Luck!
---
+++ cRFace.h
@@ -0,0 +1,23 @@
+/*
+ * Exported with nin10kit v1.8
+ * Invocation command was nin10kit --mode=3 --resize=40x40 cRFace images/cRFace.png 
+ * Time-stamp: Saturday 04/02/2022, 21:28:34
+ * 
+ * Image Information
+ * -----------------
+ * images/cRFace.png 40@40
+ * 
+ * All bug reports / feature requests are to be filed here https://github.com/TricksterGuy/nin10kit/issues
+ */
+
+#ifndef CRFACE_H
+#define CRFACE_H
+
+extern const unsigned short cRFace[1600];
+#define CRFACE_SIZE 3200
+#define CRFACE_LENGTH 1600
+#define CRFACE_WIDTH 40
+#define CRFACE_HEIGHT 40
+
+#endif
+
