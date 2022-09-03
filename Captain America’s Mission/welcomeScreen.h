df79ed525a9db1bed4e280cb9fd8fd21d5f69e7b
fikremen <70618860+fikremen@users.noreply.github.com>
2022-09-02 20:37:38 -0400

    Adding captain americas mission game
    Adding all files for the interactive graphical game coded in C. This game works on a game boy advance emulator. It is a pong style game but with randomly moving "hand" target that the player must keep from reaching a prohibited "face" zone

---
+++ Captain America’s Mission/welcomeScreen.h
@@ -0,0 +1,23 @@
+/*
+ * Exported with nin10kit v1.8
+ * Invocation command was nin10kit --mode=3 --resize=240x160 welcomeScreen images/welcomeScreen.png 
+ * Time-stamp: Saturday 04/02/2022, 18:41:28
+ * 
+ * Image Information
+ * -----------------
+ * images/welcomeScreen.png 240@160
+ * 
+ * All bug reports / feature requests are to be filed here https://github.com/TricksterGuy/nin10kit/issues
+ */
+
+#ifndef WELCOMESCREEN_H
+#define WELCOMESCREEN_H
+
+extern const unsigned short welcomeScreen[38400];
+#define WELCOMESCREEN_SIZE 76800
+#define WELCOMESCREEN_LENGTH 38400
+#define WELCOMESCREEN_WIDTH 240
+#define WELCOMESCREEN_HEIGHT 160
+
+#endif
+
